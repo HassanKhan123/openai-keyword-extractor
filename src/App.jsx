389ccf304113d286efb92be33a0aed4c655f30c6
HassanKhan123 <hk2602809@gmail.com>
2023-03-25 14:24:22 +0500

    added: textInput component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Box, Container } from "@chakra-ui/react";
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import TextInput from "./components/TextInput";
 
 const App = () => {
   return (
     <Box bg='blue.600' color='white' height={"100vh"} paddingTop={130}>
       <Container maxW={"3xl"} centerContent>
         <Header />
+        <TextInput />
         <Footer />
       </Container>
     </Box>
